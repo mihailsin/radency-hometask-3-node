b1876f1d8603a6da6f6bd0b86b3af1eef76847b9
mihailsin <mihailsin88@gmail.com>
2022-04-15 18:51:41 +0300

    add PORT variable

--- index.ts
+++ index.ts
@@ -3,11 +3,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const PORT = process.env.PORT || 3000;
 const notesRouter = require("./src/routes/api/notes");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/notes", notesRouter);
 
-app.listen(3000, () => console.log("listening 3000"));
+app.listen(PORT, () => console.log(`listening ${PORT}`));
