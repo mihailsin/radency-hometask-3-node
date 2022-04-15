f71accfed0e72dc27a3671867ae518f5ecce5df1
mihailsin <mihailsin88@gmail.com>
2022-04-15 20:04:34 +0300

    edited validation fields

--- src/services/validationMiddleware.ts
+++ src/services/validationMiddleware.ts
@@ -6,13 +6,13 @@ const Joi = require("joi");
 module.exports = {
 	addNoteValidation: (req: Request, res: Response, next: NextFunction) => {
 		const schema = Joi.object({
-			name: Joi.string().min(3).max(15).required(),
-			created: Joi.string().min(3).max(10).required(),
-			category: Joi.string().min(3).max(15).required(),
-			content: Joi.string().min(3).max(300).required(),
+			name: Joi.string().trim().min(3).max(15).required(),
+			created: Joi.string().trim().min(3).max(10).required(),
+			category: Joi.string().trim().min(3).max(15).required(),
+			content: Joi.string().trim().min(3).max(300).required(),
 			archived: Joi.boolean().required(),
 			active: Joi.boolean().required(),
-			dates: Joi.string().min(0).max(30).required(),
+			dates: Joi.string().trim().min(0).max(30).required(),
 		});
 
 		const validationResult = schema.validate(req.body);
@@ -24,13 +24,13 @@ module.exports = {
 
 	patchNoteValidation: (req: Request, res: Response, next: NextFunction) => {
 		const schema = Joi.object({
-			name: Joi.string().min(3).max(15).optional(),
-			created: Joi.string().min(3).max(10).optional(),
-			category: Joi.string().min(3).max(15).optional(),
-			content: Joi.string().min(3).max(300).optional(),
+			name: Joi.string().trim().min(3).max(15).optional(),
+			created: Joi.string().trim().min(3).max(10).optional(),
+			category: Joi.string().trim().min(3).max(15).optional(),
+			content: Joi.string().trim().min(5).max(300).optional(),
 			archived: Joi.boolean().optional(),
 			active: Joi.boolean().optional(),
-			dates: Joi.string().min(0).max(30).optional(),
+			dates: Joi.string().trim().min(0).max(30).optional(),
 		});
 
 		const validationResult = schema.validate(req.body);
